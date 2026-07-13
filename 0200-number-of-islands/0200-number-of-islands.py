3bb22bd25b6349ff2b19f67af4b347f715e79999
hongseokyun <147471374+hongseokyun@users.noreply.github.com>
2026-07-13 13:49:15 +0900

    Time: 255 ms (22.13%), Space: 27.6 MB (86.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,25 @@
 class Solution(object):
     def numIslands(self, grid):
-        n,m = len(grid),len(grid[0])
-        visit = [[False for _ in range(m)] for _ in range(n)]
+        if not grid or not grid[0]:
+            return 0
+        n, m = len(grid), len(grid[0])
         dyx = [(1,0),(0,1),(-1,0),(0,-1)]
         cnt = 0
-        arr = []
 
-        def search(grid,visit) :
-            if 0<=y<n and 0<=x<m :
-                if (grid[y][x] == "1") and (visit[y][x] == False) :
-                    return True
-            return False
+        def ok(y, x):
+            return 0 <= y < n and 0 <= x < m and grid[y][x] == "1"
 
-        for i in range(n) :
-            for j in range(m) :
-                y,x = i,j
-                if search(grid,visit) :
-                    arr.append((i,j))
-                    visit[i][j] = True
+        for i in range(n):
+            for j in range(m):
+                if ok(i, j):
                     cnt += 1
-                while arr :
-                    di,dj = arr.pop()
-                    for dy,dx in dyx :
-                        y,x = di+dy, dj+dx
-                        if search(grid,visit) :
-                            visit[y][x] = True
-                            arr.append((y,x))
+                    grid[i][j] = "0"          # 방문 표시를 grid 자체에
+                    stack = [(i, j)]
+                    while stack:
+                        ci, cj = stack.pop()
+                        for dy, dx in dyx:
+                            ny, nx = ci + dy, cj + dx
+                            if ok(ny, nx):
+                                grid[ny][nx] = "0"
+                                stack.append((ny, nx))
         return cnt
