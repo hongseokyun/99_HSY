d11da6ebfd7e75cb5c5513e6102f5d259692a1f8
hongseokyun <147471374+hongseokyun@users.noreply.github.com>
2026-08-03 15:29:40 +0900

    Time: 3 ms (90.49%), Space: 12.5 MB (42.51%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        words = set(wordDict)
+        queue = [0]          # 시작 인덱스
+        visited = set()
+
+        while queue:
+            start = queue.pop(0)
+            if start == len(s):
+                return True
+            if start in visited:
+                continue
+            visited.add(start)
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in words:
+                    queue.append(end)
+        return False
